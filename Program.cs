9c598857e0604f48a9cdf69c6bfbf75cf4709a82
Ayşegül ÖZMEN <aysegulozmn@outlook.com>
2023-11-11 15:46:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("*****");
+Console.WriteLine("*****");
+Console.WriteLine("*****");
+Console.WriteLine("*****");
+Console.WriteLine("*****");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("*");
+Console.WriteLine("*");
+Console.WriteLine("*");
+Console.WriteLine("*");
+Console.WriteLine("*");
